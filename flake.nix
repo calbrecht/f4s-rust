6855f8bb61e11e2131e367cd828f9d1c764bff07
Christian Albrecht <christian.albrecht@mayflower.de>
2021-03-30 12:23:02 +0200

    Add defaultPackage

--- flake.nix
+++ flake.nix
@@ -8,13 +8,23 @@
     };
   };
 
-  outputs = inputs: {
-    overlay = final: prev: {
-      nixpkgs-mozilla = (prev.nixpkgs-mozilla or { }) //
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      };
+    in
+    {
+      defaultPackage."${system}" = pkgs.rustNightly.rust;
+
+      overlay = final: prev: {
+        nixpkgs-mozilla = (prev.nixpkgs-mozilla or { }) //
         (import inputs.nixpkgs-mozilla final prev);
 
-      # https://rust-lang.github.io/rustup-components-history/
-      rustNightly = (prev.rustNightly or { }) //
+        # https://rust-lang.github.io/rustup-components-history/
+        rustNightly = (prev.rustNightly or { }) //
         final.nixpkgs-mozilla.rustChannelOf {
           sha256 = "sha256-9wp6afVeZqCOEgXxYQiryYeF07kW5IHh3fQaOKF2oRI=";
           date = "2021-01-01";
@@ -22,6 +32,6 @@
           #sha256 = "sha256-0BuV+blSz4OrplGvWKmeTq6fDlFPLJ3rZWkO5+HSAHM=";
           #date = "2020-10-17";
         };
+      };
     };
-  };
 }
