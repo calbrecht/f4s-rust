ad7a69f0096720867f4ac3d9f1fdaf348a203d0c
Christian Albrecht <christian.albrecht@mayflower.de>
2021-08-07 23:04:24 +0200

    update rust-nightly

--- flake.nix
+++ flake.nix
@@ -26,8 +26,10 @@
         # https://rust-lang.github.io/rustup-components-history/
         rustNightly = (prev.rustNightly or { }) //
         (import inputs.nixpkgs-mozilla final prev).rustChannelOf {
-          sha256 = "sha256-9wp6afVeZqCOEgXxYQiryYeF07kW5IHh3fQaOKF2oRI=";
-          date = "2021-01-01";
+          #sha256 = "sha256-9wp6afVeZqCOEgXxYQiryYeF07kW5IHh3fQaOKF2oRI=";
+          sha256 = "sha256-CU7J0ynkVTUuJ93IszuCHrDMaivAq3jZbDMfgjBLOBA=";
+          date = "2021-08-04";
+          #date = "2021-01-01";
           channel = "nightly";
           #sha256 = "sha256-0BuV+blSz4OrplGvWKmeTq6fDlFPLJ3rZWkO5+HSAHM=";
           #date = "2020-10-17";
