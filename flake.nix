af53ba2501906ccc68317f7ef447c55a6c1c5d57
Christian Albrecht <christian.albrecht@mayflower.de>
2021-11-13 23:14:15 +0100

    Cleanup for "no import from derivation"

--- flake.nix
+++ flake.nix
@@ -13,19 +13,19 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [ self.overlay ];
+        overlays = [
+          (import (inputs.nixpkgs-mozilla + /rust-overlay.nix))
+          self.overlay
+        ];
       };
     in
     {
       defaultPackage."${system}" = pkgs.rustNightly.rust;
 
       overlay = final: prev: {
-        nixpkgs-mozilla = (prev.nixpkgs-mozilla or { }) //
-        (import inputs.nixpkgs-mozilla final prev);
-
         # https://rust-lang.github.io/rustup-components-history/
-        rustNightly = (prev.rustNightly or { }) //
-        (import inputs.nixpkgs-mozilla final prev).rustChannelOf {
+        rustNightly = (prev.rustNightly or { })
+        // (prev.rustChannelOf or pkgs.rustChannelOf) {
           #sha256 = "sha256-9wp6afVeZqCOEgXxYQiryYeF07kW5IHh3fQaOKF2oRI=";
           sha256 = "sha256-CU7J0ynkVTUuJ93IszuCHrDMaivAq3jZbDMfgjBLOBA=";
           date = "2021-08-04";
